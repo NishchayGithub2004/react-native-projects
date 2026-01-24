7bd6e8dd83f33b4a08e5ffa48bf587af2ca070b7
Nishchay Goswami <nishchaygoswami2004@gmail.com>
2026-01-24 18:03:03 +0530

    Added e_commerce project

--- e_commerce/mobile/app/_layout.tsx
+++ e_commerce/mobile/app/_layout.tsx
@@ -1,5 +1,13 @@
 import { Stack } from "expo-router";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import '../global.css'
+
+const PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <Stack />
+    </ClerkProvider>
+  );
 }
